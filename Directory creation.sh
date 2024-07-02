b884604178450e046b6e10e1d2e0e242dba5b887
SanthoshKumarRaju <109153740+SanthoshKumarRaju@users.noreply.github.com>
2024-07-02 11:35:16 +0530

    Added the description for the each command

--- Directory creation.sh
+++ Directory creation.sh
@@ -5,7 +5,11 @@
 # This is for creating the directory along with files
 # Version: V1 
 #####################
+#create the directory
 mkdir scripts
+# Change the directory permissions
 chmod 777 scripts
+#change the directory
 cd scripts
+#Create the file in a specific directory.
 touch file1,file2
