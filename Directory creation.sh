e66420fab8c2a5b096c7b4df4749cddcdd02a621
SanthoshKumarRaju <109153740+SanthoshKumarRaju@users.noreply.github.com>
2024-07-02 11:32:04 +0530

    Basic Script written
    I wrote the basic script to create the directory along with 2 files.

--- Directory creation.sh
+++ Directory creation.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+######################
+# Author: Santhosh
+# Date: 02/07/2024
+# This is for creating the directory along with files
+# Version: V1 
+#####################
+mkdir scripts
+chmod 777 scripts
+cd scripts
+touch file1,file2
