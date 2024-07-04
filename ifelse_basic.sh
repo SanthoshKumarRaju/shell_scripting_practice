25f5f4f9c3c4b3af03119cb57247e7bb5303bafd
SanthoshKumarRaju <109153740+SanthoshKumarRaju@users.noreply.github.com>
2024-07-04 14:54:04 +0530

    basic ifelse program

---
+++ ifelse_basic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###################
+#Author: SANTHOSH
+#date: 4/07/2024
+# This script defines the basic if else statements
+#####################
+a=10
+b=20
+if [$a>$b]
+then
+  echo"A is greater tha B"
+else
+  echo"B is greater tha A"
+fi
